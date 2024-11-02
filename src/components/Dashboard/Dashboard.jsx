976f431aadf0f714d909fd7a3ae9b93fc7aff667
KanchonBauri <kanchonbauri1998@gmail.com>
2024-11-02 09:27:49 +0600

    navbar setup 47-2

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2>THis is Deshboard</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
